087663a24640b4a8839042c3960dbdf1dfbc0950
Jen Lampton <jen@jeneration.com>
2012-09-04 21:59:26 -0700

    modules page cleanup, changes to grid template

--- sites/all/themes/baddercamp/templates/views-view-grid.tpl.php
+++ sites/all/themes/baddercamp/templates/views-view-grid.tpl.php
@@ -13,7 +13,7 @@
 <?php if (!empty($title)) : ?>
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
-<div class="<?php print $class; ?>"<?php print $attributes; ?>>
+<div class="<?php print $class; ?> clear-block"<?php print $attributes; ?>>
   <?php foreach ($rows as $row_number => $columns): ?>
     <?php
       $row_class = 'row-' . ($row_number + 1);
@@ -26,7 +26,7 @@
     ?>
     <div class="views-row <?php print $row_class; ?>">
       <?php foreach ($columns as $column_number => $item): ?>
-        <div class="<?php print $column_classes[$row_number][$column_number]; ?>">
+        <div class="views-cell <?php print $column_classes[$row_number][$column_number]; ?>">
           <?php print $item; ?>
         </div>
       <?php endforeach; ?>
