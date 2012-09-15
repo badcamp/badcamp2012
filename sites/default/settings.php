bb17743e69347806752fd3107ed7a1e84e8d9ca7
Jen Lampton <jen@jeneration.com>
2012-09-14 20:38:33 -0700

    prevent notices on pantheon?

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -165,6 +165,7 @@ ini_set('session.use_cookies',      1);
 ini_set('session.use_only_cookies', 1);
 ini_set('session.use_trans_sid',    0);
 ini_set('url_rewriter.tags',        '');
+ini_set('error_reporting', E_ALL & !E_NOTICE);
 
 /**
  * If you encounter a situation where users post a large amount of text, and
