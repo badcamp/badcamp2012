6edb06dc0f0930e75bd564359db5ef9cce495483
Meghan Sweet <meghan.sweet@gmail.com>
2012-10-18 17:55:53 -0700

    custom grid tpl file

---
+++ sites/all/themes/baddercamp/templates/page-grid.tpl.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
+<head>
+  <title><?php print $head_title; ?></title>
+  <?php print $head; ?>
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+</head>
+<body class="<?php print $classes; ?> page-program-schedule">
+<?php print $content; ?>
+</div>
+</body>
+</html>
